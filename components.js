943f00aea190a0e053722fc581b17ea7718b0ac1
Robert-Stefan Musteata <87149788+robertstefanmus@users.noreply.github.com>
2026-08-24 09:35:41 +0100

    Update components.js

--- components.js
+++ components.js
@@ -1,1 +1,31 @@
 
+document.addEventListener("DOMContentLoaded", async()=> {
+    const header = document.getElementById("header");
+    const footer = document.getElementById("footer");
+
+    if(header){
+        try{
+            const response = await fetch("header.html");
+            if(!response.ok){
+                throw new Error('Failed to load header: ${response.status}');
+            }
+            header.innerHTML = await response.text();
+        }
+        catch(error){
+            console.error("Error loading header:", error);
+        }
+    }
+
+    if(footer){
+        try{
+            const response = await fetch("footer.html");
+            if(!response.ok){
+                throw new Error('Failed to load footer: ${response.status}');
+            }
+            footer.innerHTML = await response.text();
+        }
+        catch(error){
+            console.error("Error loading footer:", error);
+        }
+    }
+})
